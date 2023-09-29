b368615f1b27a1e80f5f1e7d14367f803f56a830
Joakim Brorsson <b.joakim@gmail.com>
2023-09-29 08:47:23 +0200

    helpers formatting

--- testcli/helpers.swift
+++ testcli/helpers.swift
@@ -14,63 +14,93 @@ let smallCurve = true
 let notRandom = false
 
 let domain: Domain = {
+    
     if smallCurve {
+        
         do {
+            
             return try Domain.instance(name: "EC29", p: BInt(29), a: BInt(4), b: BInt(20), gx: BInt(1), gy: BInt(5), order: BInt(37), cofactor: 1)
+            
         } catch {
+            
             print("domain error")
             return Domain.instance(curve: .EC256r1)
+            
         }
+        
     } else {
+        
         return Domain.instance(curve: .EC256r1)
+        
     }
+    
 }()
 
 let byteBufLen: Int = {
+    
     if smallCurve {
+        
         return 1
+        
     } else {
+        
         return 32
+        
     }
+    
 }()
 
 // Rand int mod p
 func randZp() -> BInt {
+    
     if notRandom {
+        
         return BInt(5)
+        
     } else {
+        
         return (domain.order - BInt.ONE).randomLessThan() + BInt.ONE
+        
     }
+    
 }
 
 func toPoint(_ x: BInt) throws -> Point {
     return try domain.multiplyPoint(domain.g, x)
 }
 
+//we represent a curve point (x,y) as bytes(x)||bytes(y) (uncompressed point representation)
 func toBytes(_ point: Point) -> Array<UInt8> {
-    //we represent a curve point (x,y) as bytes(x)||bytes(y) (uncompressed point representation)
     
     var xBytes: Bytes = point.x.asSignedBytes()
     var yBytes: Bytes = point.y.asSignedBytes()
     
     //pad if needed
     while xBytes.count < byteBufLen {
+        
         xBytes.insert(UInt8(0), at: 0)
+        
     }
+    
     while yBytes.count < byteBufLen {
+        
         yBytes.insert(UInt8(0), at: 0)
+        
     }
     
     //trim if needed
     var first = xBytes.count - byteBufLen
     var last = xBytes.count - 1
     var trimmedXBytes = Array<UInt8>(xBytes[first...last])
+    
     first = yBytes.count - byteBufLen
     last = yBytes.count - 1
     var trimmedYBytes = Array<UInt8>(yBytes[first...last])
+    
     let bytes = trimmedXBytes + trimmedYBytes
     
     return bytes
+    
 }
 
 func sha256(_ bytes: Array<UInt8>) -> BInt {
